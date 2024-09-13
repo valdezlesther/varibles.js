9020d8c6ce4f875786b1a38edeb97f3dc530e044
valdezlesther <marklesthervaldez52@gmail.com>
2024-09-13 19:50:11 +0800

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,12 @@
+let name = "Mark Lesther Valdez";
+
+ let WhatDoYouWannaBecomeInYourLife = "programmer"
+
+ let gender = "male";
+
+ let twitterhandle = "@lesther_valdez1025";
+
+ console.log(name);
+ console.log(WhatDoYouWannaBecomeInYourLfe);
+ console.log(gender);
+ console.log(twitterhandle);
